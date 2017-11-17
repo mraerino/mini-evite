4b1f514400ae4a5bc8a0417ea41b6d15d4588a85
Marcus Weiner <marcus.weiner@gmail.com>
2017-11-17 17:06:55 +0100

    :art: Strongly type default element factory

--- src/util/defaults.ts
+++ src/util/defaults.ts
@@ -2,7 +2,7 @@ import { connect as fitConnect, withExtended, FitElement } from 'fit-html';
 import { bindActionCreators } from 'redux';
 export { html } from "fit-html/dist/with-extended";
 
-export function connect(stateToProps, actions, template) {
+export function connect<S, P, OP = {}>(stateToProps, actions, template): FitElement<S, P, OP> {
     return withExtended(fitConnect(
         stateToProps,
         dispatch => bindActionCreators(actions, dispatch),
